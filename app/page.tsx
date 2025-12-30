d00eaae385415f7993fc36a7281793cdfcd82245
Omar <omar@Omars-MacBook-Pro.local>
2025-12-30 22:57:25 +0100

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
-import Link from "next/link";
+import { NavigationMenuDemo } from "@/components/nav-menu";
 
 export default function Page() {
   return (
-    <>
-      <div>Cedar app</div>
-      <ul>
-        <li>
-          <Link href="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/services">Services</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-      </ul>
-    </>
+    <div className="flex justify-center p-3">
+      <NavigationMenuDemo />
+    </div>
   );
 }
 
---
+++ components/nav-menu.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react";
+
+import { useIsMobile } from "@/hooks/use-mobile";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+export function NavigationMenuDemo() {
+  const isMobile = useIsMobile();
+
+  return (
+    <NavigationMenu viewport={isMobile}>
+      <NavigationMenuList className="flex-wrap">
+        <NavigationMenuItem className="hidden md:block">
+          <NavigationMenuTrigger>Next.js Routes</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-50 gap-4">
+              <li>
+                <NavigationMenuLink asChild>
+                  <Link href="/">Home</Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <Link href="/dashboard">Dashboard</Link>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="hidden md:block">
+          <NavigationMenuTrigger>Webflow Routes</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-50 gap-4">
+              <li>
+                <NavigationMenuLink asChild>
+                  <Link href="/about">About</Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <Link href="/services">Services</Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <Link href="/contact">Contact</Link>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
