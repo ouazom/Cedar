5c88094b2ada60cd6b3bcc85087780086c06a205
Omar <omar@Omars-MacBook-Pro.local>
2025-12-30 22:05:22 +0100

    testing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typedRoutes: true,
+  async rewrites() {
+    return {
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: "/:path*",
+          destination: `https://new.1web.com/:path*`,
+          has: [{ type: "header", key: "User-Agent" }],
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
